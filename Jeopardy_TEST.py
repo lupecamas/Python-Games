0e4a54f0da752d0ffbb7f9eecc71242dfb365cd9
lupecamas <46791315+lupecamas@users.noreply.github.com>
2019-03-05 10:40:30 -0700

    Add files via upload

---
+++ Jeopardy_TEST.py
@@ -0,0 +1,122 @@
+score = 0
+print ("In this game you be tested in your rap knowlege")
+print ("Directions:This a Jeopardy. How to answer this question")
+print (' Answer question like this type " who is your answer or just the name')
+
+print ("If you want to play type rap or old rock")
+
+
+
+answer= input().lower()
+if answer == "rap":
+     print("Was off the Remy, had a Papoose")
+     print("Had to hit my old town to duck the news")
+     print("Two-four hour lockdown, we made no moves")
+     print("Now it's 4AM and I'm back up poppin' with the crew")
+     print("I just landed in, Chase B mixes pop like Jamba Juice")
+     print("Different colored chains, think my jeweler really sellin' fruits")
+     print("And they chokin', man, know the crackers wish it was a noose")
+     print("----------------------------------------------------------------")
+     print("Who is Kodak Black")
+     print("Who is Offset")
+     print("Who is Travis Scott")
+answer = input().lower()
+if answer == "who is travis scott":
+    score = score +1
+    print("Score:" + str (score))
+    print("I pull up, I pull up, I pull up")
+    print("I hop out with all of the drugs in the cooler (skrt)")
+    print("I'm cookin', I'm cookin', I'm whippin', I'm whippin' into a rock up, let it lock up lock up")
+    print("-----------------------------------------------")
+    print("Who is offset")
+    print("Who is Migos")
+    print("Who is Young Rich")
+    answer = input().lower()
+    if answer == "who is offset" or answer == "offset":
+            score = score + 1
+            print("Score:" + str (score))
+            print("Little somethin' for my godson Elijah and a little girl named Corinne")
+            print("Some say the blacker the berry, the sweeter the juice")
+            print("I say the darker the flesh then the deeper the roots")
+            print("-------Option---------------")
+            print("Who is B.I.G")
+            print("Who is OutKast")
+            print("Who is Tupac")
+            answer = input().lower()
+            if answer == "who is tupac" or answer == "tupac":
+                 score = score +1
+                 print("Score:" + str (score))
+                 print("How'd it get so scandalous?")
+                 print("Oh, how'd it get so scandalous?")
+                 print("Oh, oh, how'd it get, how'd it get")
+                 print("How'd it get so scandalous?")
+                 print("How'd it get so scandalous?")
+                 print("-------Option---------------")
+                 print("Who is Childish Gambino")
+                 print("Who is Kid Cudi")
+                 print("Who is Sheck Wes")
+                 answer = input().lower()
+                 if answer == "who is childish gambino" or answer == "childish gambino":
+                     score = score +1
+                     print("Score:" + str (score))
+                     print("I gave her everything")
+                     print("She took my heart and left me lonely")
+                     print("I've been broken, heart's contentious")
+                     print("I won't fix, I'd rather weep")
+                     print("-------Option---------------")
+                     print("Who is Drake")
+                     print("Who is 2 Chainz")
+                     print("Who is xxxtentacion")
+                     answer = input().lower()
+                     if answer == "who is xxxtentacion" or answer == "xxxtentacion":
+                         score = score +1
+                         print("Score:" +str (score))
+                         print("Bad things")
+                         print("It's a lot of bad things")
+                         print("That they wishin' and wishin' and wishin' and wishin'")
+                         print("They wishin' on me")
+                         print("-------Option---------------")
+                         print("Who is Future")
+                         print("Who is Chance the rapper")
+                         print("Who is Drake")
+                         answer = input().lower()
+                         if answer == " who is drake" or "drake":
+                             score = score +1
+                             print("Score:" + str (score))
+                             print(" Final Score:" + str (score))
+                             print("Try old rock")
+                         
+                
+                
+  
+          
+
+else:
+    print("Now I lay me down to sleep")
+    print("Now I lay me down to sleep")
+    print("Pray the lord my soul to keep")
+    print("Pray the lord my soul to keep")
+    print("-------Option---------------")
+    print("Who is Metallica")
+    print("Who is Black Sabbath")
+    print("Who is Disturbed")
+    answer = input().lower()
+    if answer == "who is metallica":
+        score = score +1
+        print("Score:" + str (score))
+        print("Nobody wants him")
+        print("He just stares at the world")
+        print("Planning his vengeance")
+        print("That he will soon unfold")
+        print("-------Option---------------")
+        print(" Who is Iron Man")
+        print("Who is Black Sabbath")
+        print("Who is Metallica")
+        answer == input().lower()
+        if answer == "who is iron man" or answer == "iron man":
+             score = score +1
+             print("Score:" + str (score))
+             print("Finnal Score:" + str (score))
+
+
+
